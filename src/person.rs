e5180d73db4c4b2440383543ab4dfdb54b70d764
Herman J. Radtke III <herman@hermanradtke.com>
2016-10-20 23:00:46 -0700

    Create Person type

---
+++ src/person.rs
@@ -0,0 +1,34 @@
+use name::NameString;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Person {
+    pub name: NameString,
+}
+
+impl Person {
+    pub fn new<I>(n: I) -> Person
+        where I: Into<NameString>
+    {
+        Person {
+            name: n.into()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use name::{NameStr, NameString};
+
+    #[test]
+    fn test_person_new() {
+        let o_name = NameString::from_str("Name");
+        let p1 = Person::new(o_name);
+
+        let b_name = NameStr::new("Name");
+        let p2 = Person::new(b_name);
+
+        assert_eq!(p1, p2);
+    }
+}
+
