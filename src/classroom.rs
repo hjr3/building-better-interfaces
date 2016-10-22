eb2bd3e0af9edf5162ee103e5339493f41058f24
Herman J. Radtke III <herman@hermanradtke.com>
2016-10-21 21:36:00 -0700

    Added `Classroom::as_roster()` example

--- src/classroom.rs
+++ src/classroom.rs
@@ -1,4 +1,5 @@
 use name::NameString;
+use roster::Roster;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Classroom {
@@ -23,12 +24,22 @@ impl Classroom {
     {
         self.names.push(name.into())
     }
+
+    pub fn as_roster(&self) -> Roster {
+        let mut r = Roster::new();
+
+        for name in self.names.iter() {
+            r.add_name(name);
+        }
+
+        r
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use {NameStr, NameString};
+    use name::{NameStr, NameString};
     use roster::Roster;
 
     #[test]
@@ -48,5 +59,38 @@ mod test {
     fn test_classroom_add_name_using_into() {
         let mut c = Classroom::new();
         c.add_name(NameStr::new("Name"));
+    }
+
+    #[test]
+    fn test_classroom_with_names() {
+        let names = vec![
+            NameString::from_str("Name1"),
+            NameString::from_str("Name2"),
+        ];
+
+        let given = Classroom::with_names(names);
+
+        let mut expected = Classroom::new();
+        expected.add_name(NameString::from_str("Name1"));
+        expected.add_name(NameString::from_str("Name2"));
+
+        assert_eq!(expected, given);
+    }
+
+    #[test]
+    fn test_classroom_as_roster() {
+        let names = vec![
+            NameString::from_str("Name1"),
+            NameString::from_str("Name2"),
+        ];
+
+        let c = Classroom::with_names(names);
+        let given = c.as_roster();
+
+        let mut expected = Roster::new();
+        expected.add_name(NameStr::new("Name1"));
+        expected.add_name(NameStr::new("Name2"));
+
+        assert_eq!(expected, given);
     }
 }
